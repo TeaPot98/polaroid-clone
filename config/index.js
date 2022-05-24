3dc66659c56de629ca5340b0854e4f1df3a8af7e
TeaPot98 <trifautan@gmail.com>
2022-05-24 22:43:08 +0300

    changed domain address to a valid one from Vercel

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://samplewebsite.com'
+export const server = dev ? 'http://localhost:3000' : 'polaroid-clone-9pv0u3w71-teapot98.vercel.app'
